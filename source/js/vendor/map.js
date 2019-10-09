a2aa362edd34a4889fd0739466fd6b1c924d22dc
mal1911 <ma1911@mail.ru>
2019-10-10 00:30:08 +0300

    исправляем ошибки

--- source/js/vendor/map.js
+++ source/js/vendor/map.js
@@ -3,17 +3,6 @@
   /* map */
   ymaps.ready(init);
 
-  ymaps.addListener('load' , function (evt) {
-    var iframes = document.querySelectorAll('iframe');
-    console.log(iframes);
-    if (iframes) {
-      for (var i = 0; i < iframes.length; i++) {
-        iframes[i].setAttribute('title', 'titleString');
-        console.log(iframes[i]);
-      }
-    }
-  });
-
   function init() {
     var myMap = new ymaps.Map('map__container', {
       center: [55.631968, 37.618355],
@@ -26,6 +15,4 @@
     }, {preset: 'islands#greenIcon'});
     myMap.geoObjects.add(myPlacemark);
   }
-
-
 })();
